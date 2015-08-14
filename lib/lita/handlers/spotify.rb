7f0a4acf4c7c1c50ae016416cb54bdf6b58665b2
Bipol Alam <bipolalam@gmail.com>
2015-08-14 15:49:49 +0000

    fixed the logic, a lot of syntax errors as well as improper json iterable

--- lib/lita/handlers/spotify.rb
+++ lib/lita/handlers/spotify.rb
@@ -1,29 +1,30 @@
 module Lita
   module Handlers
     class Spotify < Handler
-    	_API_URL = "https://api.spotify.com/v1/"
-      _TRACK_URL = _API_URL + "tracks/"
-      _SEARCH_URL = _API_URL + "search/"
+      API_URL = "https://api.spotify.com/v1/"
+      SEARCH_URL = API_URL + "search/"
 
       route(/^(?:spotify)?\s+(.*)/i, :find_song, command: true, help: {
          "spotify artistName,songName" => "Retrieves a spotify link for an artist and songName"
       })
 
       def find_song(res)
-        res = res.split(',')
-        artistName = res[0]
-        songName = res[1]
+        res_string = res.matches[0][0].split(',')
+        artistName = res_string[0]
+        songName = res_string[1]
         track_res = http.get(
-          "#{_SEARCH_URL}",
+          SEARCH_URL,
           q: songName,
-          type: "tracks"
+          type: "track"
         )
 
-        if track_res != 200
+        if track_res.status != 200
           return
 	end
 
-        track_url = MultiJson.load(track_res.body)["items"].select{|t| t['artists']['name'] == artistName && t['name'] == songName}.first['external_urls']['spotify']
+	
+        #track_url = MultiJson.load(track_res.body)["tracks"].select{|t| t['artists']['name'] == artistName}.first['external_urls']['spotify']
+        track_url = MultiJson.load(track_res.body)["tracks"]["items"].select{ |t| t["artists"][0]["name"] == artistName }.first['external_urls']['spotify']
         res.reply track_url
       end
     end
